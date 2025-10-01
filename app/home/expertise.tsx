63505d7af0c78ec9f69798aff3c25277a8bcc411
Getachew Abegaz <getabegaz@gmail.com>
2025-10-01 17:53:03 +0300

    edited expertise

--- app/home/expertise.tsx
+++ app/home/expertise.tsx
@@ -1,12 +1,13 @@
 const expertise = [
+   {
+    title: 'Research & Policy Analysis',
+    description: 'Conducting in-depth research on emerging markets, synthesizing complex information, and providing clear analysis for policymakers and investors.'
+  },
   {
     title: 'Macro & Financial Modeling',
     description: 'Developing econometric and statistical models to forecast market trends, assess risk, and analyze the impact of economic policy.'
   },
-    {
-    title: 'Research & Policy Analysis',
-    description: 'Conducting in-depth research on emerging markets, synthesizing complex information, and providing clear analysis for policymakers and investors.'
-  },
+   
   {
     title: 'Data-Driven Strategy',
     description: 'Translating raw data from diverse sources into clear, actionable insights that inform investment theses and strategic business decisions.'
